047e9764a6f9807fd0a04a4575d69726e9dec2b6
VictorDeLaPresilla <78715249+VictorDeLaPresilla@users.noreply.github.com>
2023-05-18 17:52:43 +0000

    tabla con formato

---
+++ auakaadmin/src/Tabla.js
@@ -0,0 +1,21 @@
+import styles from "./Tabla.module.css"
+export function Tabla(){
+    return(
+        <table>
+        <thead > 
+        <tr>
+                <th>id</th>
+                <th>producto</th>
+                <th>detalles</th>
+                <th>precio</th>
+                <th>entrega</th>
+                <th>fotografia</th>
+                <th>visible</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+    );
+}
+export default Tabla;
